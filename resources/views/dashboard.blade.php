c7cc4487c300ef0ce17e719278fb9fc40689b9af
Raimonds Nalezitijs <102956031+RaymondNalezitij@users.noreply.github.com>
2022-09-25 22:45:11 +0300

    Fixed some front end issues deriving from administrator checks

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,10 +14,10 @@
                     <div style="display: flex; flex-direction: row">
 
                         <select name="category" id="category">
-                            <option onselect="sortByCategory(1)" value="all">All</option>
+                            <option value="all">All</option>
                             @foreach($categories as $category)
                                 @if($category->id > 1)
-                                    <option onselect="sortByCategory({{ $category->id }})" value="{{ $category->name }}">{{ $category->name }}</option>
+                                    <option value="{{ $category->name }}">{{ $category->name }}</option>
                                 @endif
                             @endforeach
                         </select>
@@ -62,17 +62,15 @@
                         </div>
                     @endif
 
-
                     <div style="display: none">{{ $i=1 }}</div>
                     @foreach($posts as $post)
                         <div id="postNumber{{ $post->id }}">
                             <div class="py-6 m-6 bg-white border-b border-t border-gray-200">
-
-                                @if(count($admin_status) > 0)
-                                    @if($admin_status[0]->is_admin_at == 'global' || $admin_status[0]->is_admin_at == 'dashboard')
-                                        <div style="display: flex; flex-direction: row; justify-content: space-between">
-                                            <form action="{{ route('dashboard.store') }}" method="POST">
-                                                @csrf
+                                <div style="display: flex; flex-direction: row; justify-content: space-between">
+                                    <form style="width: 100%" action="{{ route('dashboard.store') }}" method="POST">
+                                        @csrf
+                                        @if(count($admin_status) > 0)
+                                            @if($admin_status[0]->is_admin_at == 'global' || $admin_status[0]->is_admin_at == 'dashboard')
                                                 <label for="postId"></label><input name="postId" id="postId"
                                                                                    value="{{ $post->id }}"
                                                                                    style="display: none">
@@ -85,107 +83,121 @@
                                                             id="deletePost">Remove post
                                                     </button>
                                                 </div>
-                                                @endif
-                                                @endif
-                                                <div id="postEdit{{ $i+1 }}" style="display: none">
-                                                    <label for="postEditText{{ $i+1 }}">
+                                            @endif
+                                        @endif
+                                        <div id="postEdit{{ $i+1 }}" style="display: none">
+                                            <label for="postEditText{{ $i+1 }}">
                                                     <textarea class="editTextBox" id="postEditText{{ $i+1 }}"
                                                               name="postEditText"></textarea>
-                                                    </label>
-                                                    <button type="submit" name="editPost" id="editPost">Edit post</button>
-                                                </div>
-                                                <div style="display: flex;flex-direction: row">
-                                                    <p class="postText" id="postText{{ $i+1 }}">{{ $post->text }}</p>
-                                                    @foreach($categories as $category)
-                                                        @if($category->id == $post->category_id)
-                                                            <p style="font-size: 10px">{{ $category->name }}</p>
-                                                        @endif
-                                                    @endforeach
-                                                </div>
-                                            </form>
-                                            <form action="{{ route('dashboard.store') }}" method="POST">
-                                                @csrf
-                                                <div style="display: flex;flex-direction: column">
-                                                    <select name="category" id="category" style="max-height: 40px">
-                                                        <option value="all">All</option>
-                                                        @foreach($categories as $category)
-                                                            @if($category->id > 1)
-                                                                <option
-                                                                    value="{{ $category->name }}">{{ $category->name }}</option>
-                                                            @endif
-                                                        @endforeach
-                                                    </select>
-                                                    <label for="postId"></label><input name="postId" id="postId"
-                                                                                       value="{{ $post->id }}"
-                                                                                       style="display: none">
-                                                    <button type="submit" name="assignCategory">Assign category</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <form action="{{ route('dashboard.store') }}" method="POST">
-                                            @csrf
-                                            <label for="postId"></label><input name="postId" id="postId"
-                                                                               value="{{ $post->id }}"
-                                                                               style="display: none">
-                                            <label for="commentInput">
-                                                <input id="commentInput" name="commentInput" class="commentInput"
-                                                       type="text" placeholder="Add comment" autocomplete="off">
                                             </label>
-                                            <div class="commentSubmitContainer">
-                                                <button class="commentSubmit" type="submit" name="createComment">Add
-                                                    comment
-                                                </button>
+                                            <button type="submit" name="editPost" id="editPost">Edit post
+                                            </button>
+                                        </div>
+                                        <div
+                                            style="display: flex;flex-direction: row; justify-content: space-between">
+                                            <div style="display: flex;flex-direction: row;">
+                                                <p class="postText"
+                                                   id="postText{{ $i+1 }}">{{ $post->text }}</p>
+                                                @foreach($categories as $category)
+                                                    @if($category->id == $post->category_id)
+                                                        <p style="font-size: 10px">{{ $category->name }}</p>
+                                                    @endif
+                                                @endforeach
                                             </div>
-                                        </form>
-
-                                        @foreach($comments as $comment)
-                                            @if($comment->post_id==$post->id)
+                                            <div>
                                                 <form action="{{ route('dashboard.store') }}" method="POST">
-                                                    <div style="display: flex;"
-                                                         class="flex-row justify-between p-2 bg-white border-t border-gray-200">
-                                                        <div style="width: 90%;">
-                                                            <p class="comment"
-                                                               id="comment{{ $comment->id }}">{{ $comment->text }}</p>
-                                                            <label for="commentEditTextBox{{ $comment->id }}">
+                                                    @csrf
+                                                    <div style="display: flex;flex-direction: column">
+                                                        <select name="category" id="category"
+                                                                style="max-height: 40px">
+                                                            <option value="all">All</option>
+                                                            @foreach($categories as $category)
+                                                                @if($category->id > 1)
+                                                                    <option
+                                                                        value="{{ $category->name }}">{{ $category->name }}</option>
+                                                                @endif
+                                                            @endforeach
+                                                        </select>
+                                                        <label for="postId"></label><input name="postId"
+                                                                                           id="postId"
+                                                                                           value="{{ $post->id }}"
+                                                                                           style="display: none">
+                                                        <button type="submit" name="assignCategory">Assign
+                                                            category
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <form action="{{ route('dashboard.store') }}" method="POST">
+                                    @csrf
+                                    <label for="postId"></label><input name="postId" id="postId"
+                                                                       value="{{ $post->id }}"
+                                                                       style="display: none">
+                                    <label for="commentInput">
+                                        <input id="commentInput" name="commentInput" class="commentInput"
+                                               type="text" placeholder="Add comment" autocomplete="off">
+                                    </label>
+                                    <div class="commentSubmitContainer">
+                                        <button class="commentSubmit" type="submit" name="createComment">Add
+                                            comment
+                                        </button>
+                                    </div>
+                                </form>
+
+                                @foreach($comments as $comment)
+                                    @if($comment->post_id==$post->id)
+                                        <form action="{{ route('dashboard.store') }}" method="POST">
+                                            <div style="display: flex;"
+                                                 class="flex-row justify-between p-2 bg-white border-t border-gray-200">
+                                                <div style="width: 90%;">
+                                                    <p class="comment"
+                                                       id="comment{{ $comment->id }}">{{ $comment->text }}</p>
+                                                    <label for="commentEditTextBox{{ $comment->id }}">
                                                                 <textarea id="commentEditTextBox{{ $comment->id }}"
                                                                           name="commentEditText"
                                                                           style="width:100%; display: none;"
                                                                           autocomplete="off"></textarea>
-                                                            </label>
+                                                    </label>
+                                                </div>
+                                                @if(count($admin_status) > 0)
+                                                    @if($admin_status[0]->is_admin_at == 'global' || $admin_status[0]->is_admin_at == 'dashboard')
+                                                        @csrf
+                                                        <label for="commentId{{ $comment->id }}">
+                                                            <input name="commentId"
+                                                                   id="commentId{{ $comment->id }}"
+                                                                   value="{{ $comment->id }}"
+                                                                   style="display: none">
+                                                        </label>
+                                                        <div style="display: flex; flex-direction: column">
+                                                            <div class="adjustCommentsButtons">
+                                                                <button type="button" name="editComment"
+                                                                        onclick="showEditCommentField(document.getElementById('commentId{{ $comment->id }}').value)"
+                                                                        id="editComment{{ $comment->id }}">
+                                                                    &#9997;
+                                                                </button>
+                                                                <button type="submit" name="deleteComment"
+                                                                        id="deleteComment">&#128465;
+                                                                </button>
+                                                            </div>
+
+                                                            <button class="p-2" type="submit"
+                                                                    name="submitCommentEdit"
+                                                                    id="submitComment{{ $comment->id }}"
+                                                                    style="visibility: hidden">Commit
+                                                            </button>
                                                         </div>
-                                                        @if(count($admin_status) > 0)
-                                                            @if($admin_status[0]->is_admin_at == 'global' || $admin_status[0]->is_admin_at == 'dashboard')
-                                                                @csrf
-                                                                <label for="commentId{{ $comment->id }}">
-                                                                    <input name="commentId" id="commentId{{ $comment->id }}"
-                                                                           value="{{ $comment->id }}" style="display: none">
-                                                                </label>
-                                                                <div style="display: flex; flex-direction: column">
-                                                                    <div class="adjustCommentsButtons">
-                                                                        <button type="button" name="editComment"
-                                                                                onclick="showEditCommentField(document.getElementById('commentId{{ $comment->id }}').value)"
-                                                                                id="editComment{{ $comment->id }}">&#9997;
-                                                                        </button>
-                                                                        <button type="submit" name="deleteComment"
-                                                                                id="deleteComment">&#128465;
-                                                                        </button>
-                                                                    </div>
 
-                                                                    <button class="p-2" type="submit"
-                                                                            name="submitCommentEdit"
-                                                                            id="submitComment{{ $comment->id }}"
-                                                                            style="visibility: hidden">Commit
-                                                                    </button>
-                                                                </div>
+                                                    @endif
+                                                @endif
+                                            </div>
+                                        </form>
 
-                                                            @endif
-                                                        @endif
-                                                    </div>
-                                                </form>
-
-                                            @endif
-                                        @endforeach
-                                        <div class="border-t border-gray-200"></div>
+                                    @endif
+                                @endforeach
+                                <div class="border-t border-gray-200"></div>
                             </div>
                         </div>
                         <div style="display: none">{{ $i+=1 }}</div>
